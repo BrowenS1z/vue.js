668fb53317cf25880dee435c36e8a649199cb20a
BrowenS1z <BrowenSiz@yandex.ru>
2021-10-11 20:06:52 +0300

    lesson3
    lesson3

---
+++ lesson3/src/components/PaymentForm.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <form class="form">
+      <input type="text" placeholder="Description" v-model="category" />
+      <input type="text" placeholder="Amount" v-model="value" />
+      <input type="date" placeholder="Date" v-model="date" />
+      <button class="AddBtn" type="button" @click="addPayment">Add +</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentForm",
+  data() {
+    return {
+      category: "",
+      date: "",
+      value: null
+    };
+  },
+  methods: {
+    addPayment() {
+      const data = {
+        category: this.category,
+        value: this.value,
+        date: this.date,
+      };
+      this.$emit("addPayment", data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form{
+    box-sizing: border-box;
+    margin-top: 10px;
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+}
+.form input{
+    margin-top: 10px;
+    padding: 10px;
+}
+.AddBtn{
+    width: 150px;
+    background-color: #25A79A;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    padding: 10px 15px 10px 15px;
+    margin-top: 10px;
+}
+</style>
---
+++ lesson3/src/components/PaymentList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="addCostForm">
+        <h1>My personal costs</h1>
+        <button class="openForm" @click="isVisible = !isVisible">ADD NEW COST +</button>
+    </div>
+    <PaymentForm v-show="isVisible" @addPayment="addNewPayment" />
+    <PaymentListData :list="responseData" />
+  </div>
+</template>
+
+<script>
+import PaymentListData from "./PaymentListData";
+import PaymentForm from "./PaymentForm";
+export default {
+  name: "PaymentList",
+  components: {
+    PaymentListData,
+    PaymentForm,
+  },
+  data() {
+    return {
+      responseData: [
+        {
+          date: "2020-03-28",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "2020-03-24",
+          category: "Transport",
+          value: 368,
+        },
+        {
+          date: "2020-03-26",
+          category: "Food",
+          value: 532,
+        },
+      ],
+      isVisible: false,
+    };
+  },
+  methods: {
+    addNewPayment(data) {
+      this.responseData = [...this.responseData, data];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.addCostForm{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+.openForm{
+    width: 150px;
+    background-color: #25A79A;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    padding: 10px 15px 10px 15px;
+}
+</style>
